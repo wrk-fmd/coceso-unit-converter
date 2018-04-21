604066be1f2665fbfb41a15e2430f380dcae2acd
Robert Wittek <r.m.wittek@gmail.com>
2018-04-21 17:04:46 +0200

    create multiple contacts for multiple lines

--- src/main/java/rwi/tetra/json/converter/Converter.java
+++ src/main/java/rwi/tetra/json/converter/Converter.java
@@ -2,6 +2,8 @@ package rwi.tetra.json.converter;
 
 import com.google.common.collect.ImmutableList;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import rwi.tetra.json.converter.input.InputFormat;
 import rwi.tetra.json.converter.input.InputPerson;
 import rwi.tetra.json.converter.input.InputUnit;
@@ -10,10 +12,14 @@ import rwi.tetra.json.converter.output.OutputFormat;
 import rwi.tetra.json.converter.output.OutputUnit;
 import rwi.tetra.json.converter.output.SmsContact;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Converter {
+    private static final Logger LOG = LoggerFactory.getLogger(Converter.class);
+
     private final NumberNormalizer numberNormalizer;
 
     Converter() {
@@ -31,7 +37,7 @@ public class Converter {
     private OutputUnit convertUnit(final InputUnit inputUnit) {
         List<SmsContact> smsContacts = inputUnit.getCrew()
                 .stream()
-                .map(this::createSmsContact)
+                .flatMap(this::createSmsContact)
                 .filter(contact -> StringUtils.isNotBlank(contact.getPhoneNumber()))
                 .collect(Collectors.toList());
         List<AnalogRadioContact> radioContacts = ImmutableList.of();
@@ -42,7 +48,23 @@ public class Converter {
         return new OutputUnit(inputUnit.getId() + "", inputUnit.getCall(), radioContacts, smsContacts, ImmutableList.of());
     }
 
-    private SmsContact createSmsContact(final InputPerson person) {
-        return new SmsContact(String.format("%s %s", person.getFirstname(), person.getLastname()), numberNormalizer.normalize(person.getContact()));
+    private List<SmsContact> splitToMultiple(final SmsContact contact) {
+        String[] numbers = contact.getPhoneNumber()
+                .split("\n");
+
+        List<SmsContact> contacts = Arrays.stream(numbers)
+                .map(number -> new SmsContact(contact.getOwner(), number))
+                .collect(Collectors.toList());
+        if (contacts.size() > 0) {
+            LOG.info("Splitted contact {} to contacts {}.", contact, contacts);
+        }
+
+        return contacts;
+    }
+
+    private Stream<SmsContact> createSmsContact(final InputPerson person) {
+        return Arrays.stream(person.getContact().split("\n"))
+                .map(numberNormalizer::normalize)
+                .map(number -> new SmsContact(String.format("%s %s", person.getFirstname(), person.getLastname()), number));
     }
 }
